9ee46c0583f4e483dd8d98f82003112908906792
Lucia Viazzo <127992377+luciaviazzo@users.noreply.github.com>
2024-05-08 22:19:58 -0300

    Modo oscuro / modo claro (no funciona en home)

--- scripts/script.js
+++ scripts/script.js
@@ -198,3 +198,20 @@ function resetState() {
 }
 
 startTrivia();
+
+
+
+//MODO CLARO - MODO OSCURO
+const brush = document.querySelector(".menu .brush");
+let state = false;
+
+brush.addEventListener("click", function() {
+    if (!state) {
+        document.body.style.backgroundColor = "#FFF9F3";
+        state = true;
+    } else {
+        document.body.style.backgroundColor = "#8CA87C";
+        state = false;
+    }
+});
+
