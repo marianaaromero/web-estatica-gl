cc34e393f0cb148823865f48925cec623f53f7b4
marianaaromero <marianaayelenromero9@gmail.com>
2024-05-10 15:32:21 -0300

    gallery js

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,55 @@ brush.addEventListener("click", function() {
     }
 });
 
+// button elements
+const nextButton = document.querySelector('.next');
+const prevButton = document.querySelector('.prev');
+
+nextButton.addEventListener('click', handleNextClick);
+prevButton.addEventListener('click', handlePrevClick);
+
+function handleNextClick() {
+    const slide = document.querySelector('.slide');
+    const firstItem = slide.firstElementChild;
+    slide.appendChild(firstItem);
+}
+
+function handlePrevClick() {
+    const slide = document.querySelector('.slide');
+    const lastItem = slide.lastElementChild;
+    slide.prepend(lastItem);
+}
+
+// event: keyboard 
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowRight') {
+        handleNextClick();
+        highlightActiveButton(nextButton);
+    } else if (event.key === 'ArrowLeft') {
+        handlePrevClick();
+        highlightActiveButton(prevButton);
+    }
+});
+
+document.addEventListener('keyup', function(event) {
+    if (event.key === 'ArrowRight' || event.key === 'ArrowLeft') {
+        removeActiveButtonHighlight();
+    }
+});
+
+// highlight active button
+
+function highlightActiveButton(button) {
+
+    document.querySelectorAll('.gallery-button button').forEach(btn => {
+        btn.classList.remove('active-button');
+    });
+
+    button.classList.add('active-button');
+}
+
+function removeActiveButtonHighlight() {
+    document.querySelectorAll('.gallery-button button').forEach(btn => {
+        btn.classList.remove('active-button');
+    })
+}
