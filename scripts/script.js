ce889a978b84d4a19efe531e91295745d75fee65
Lucia Viazzo <127992377+luciaviazzo@users.noreply.github.com>
2024-05-11 22:32:01 -0300

    Agrego modo oscuro - modo claro

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,16 @@
 //MODO CLARO - MODO OSCURO
 const brush = document.querySelector(".menu .brush");
+const header = document.querySelector("header");
 let state = false;
 
 brush.addEventListener("click", function () {
     if (!state) {
         document.body.style.backgroundColor = "#FFF9F3";
+        header.style.backgroundColor = "#8CA87C";
         state = true;
     } else {
         document.body.style.backgroundColor = "#8CA87C";
+        header.style.backgroundColor = "#6D8261";
         state = false;
     }
 });
